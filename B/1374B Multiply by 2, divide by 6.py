76999c039610b300cb39f831dbe20791ab45cf6d
zielman <58646935+zielman@users.noreply.github.com>
2021-04-09 01:04:09 +0200

    Add files via upload

---
+++ B/1374B Multiply by 2, divide by 6.py
@@ -0,0 +1,22 @@
+# https://codeforces.com/problemset/problem/1374/B
+
+def main():
+    t = int(input())
+    nums = [int(input()) for i in range(t)]
+    
+    for n in nums:
+        a = 0
+        b = 0
+        while n % 2 == 0:
+            n //= 2
+            a += 1
+        while n % 3 == 0:
+            n //= 3
+            b += 1
+        if n == 1 and a <= b:
+            print(2 * b - a )
+        else:
+            print(-1)
+    
+if __name__ == '__main__':
+    main()
