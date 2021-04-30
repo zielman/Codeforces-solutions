35e954f90618f9adacece41e42050e4d79323a79
Marcin Gdowski <gdowskimarcin@gmail.com>
2021-04-30 23:29:43 +0200

    removed unnecessary code

--- A/4A Watermelon.py
+++ A/4A Watermelon.py
@@ -1,13 +1,9 @@
 # https://codeforces.com/problemset/problem/4/A
 
-def main():
-    w = int(input())
-    r = w - 2
-    
-    if r % 2 == 0 and r >= 2:
-        print('YES')
-    else:
-        print('NO')
+w = int(input())
+r = w - 2
 
-if __name__ == '__main__':
-    main()
+if r % 2 == 0 and r >= 2:
+    print('YES')
+else:
+    print('NO')
--- A/9A Die Roll.py
+++ A/9A Die Roll.py
@@ -1,20 +1,16 @@
 # https://codeforces.com/problemset/problem/9/A
 
-def main():
-    y, w = map(int, input().split())
+y, w = map(int, input().split())
 
-    numerator = 7 - max(y, w)
+numerator = 7 - max(y, w)
 
-    if numerator == 6:
-        print(f"{1}/{1}")
-    elif numerator == 4:
-        print(f"{2}/{3}")
-    elif numerator == 3:
-        print(f"{1}/{2}")
-    elif numerator == 2:
-        print(f"{1}/{3}")
-    else:
-        print(f"{numerator}/{6}")
-
-if __name__ == '__main__':
-    main()
+if numerator == 6:
+    print(f"{1}/{1}")
+elif numerator == 4:
+    print(f"{2}/{3}")
+elif numerator == 3:
+    print(f"{1}/{2}")
+elif numerator == 2:
+    print(f"{1}/{3}")
+else:
+    print(f"{numerator}/{6}")
