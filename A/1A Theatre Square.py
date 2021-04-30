b16b3304013a500f17ad7b20b7020f4753f6486d
Marcin Gdowski <gdowskimarcin@gmail.com>
2021-04-30 23:10:12 +0200

    removed unnecessary code

--- A/1A Theatre Square.py
+++ A/1A Theatre Square.py
@@ -1,12 +1,6 @@
 # https://codeforces.com/problemset/problem/1/A
 
 import math
-
-def main():
     
-    n, m, a = map(int, input().split())
-    
-    print(math.ceil(n / a) * math.ceil(m / a))    
-    
-if __name__ == '__main__':
-    main()
+n, m, a = map(int, input().split())
+print(math.ceil(n / a) * math.ceil(m / a))
