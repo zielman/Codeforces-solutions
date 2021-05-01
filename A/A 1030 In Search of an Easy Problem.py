2a6bff12573dca74742878455fb208c8f8787b04
Marcin Gdowski <gdowskimarcin@gmail.com>
2021-05-01 06:39:34 +0200

    removed unnecessary code

--- A/A 1030 In Search of an Easy Problem.py
+++ A/A 1030 In Search of an Easy Problem.py
@@ -1,13 +1,9 @@
 # https://codeforces.com/problemset/problem/1030/A
 
-def main():
-    n = int(input())
-    o = list(map(int, input().split()))
+n = int(input())
+o = list(map(int, input().split()))
 
-    if o.count(1) != 0:
-        print('HARD')
-    else:
-        print('EASY')
-
-if __name__ == '__main__':
-    main()
+if o.count(1) != 0:
+    print('HARD')
+else:
+    print('EASY')
