89ccfbcdbafc37d9ea786aec18aa63f99065bc66
Zielman <gdowskimarcin@gmail.com>
2021-05-02 17:45:15 +0200

    Clean-up

--- A/A 69 Young Physicist.py
+++ A/A 69 Young Physicist.py
@@ -1,28 +1,16 @@
 # https://codeforces.com/problemset/problem/69/A
 
-coordinates = []
+n = int(input())
+coordinates = [list(map(int, input().split())) for _ in range(n)]
 
-def get_input():
-    coordinates.append(input()) 
-    while len(coordinates) - 1 < int(coordinates[0]):
-        get_input()
+x, y, z = 0, 0, 0
 
-get_input()
-coordinates.pop(0)
+for c in coordinates:
+    x += c[0]
+    y += c[1]
+    z += c[2]
 
-x = 0
-y = 0
-z = 0
-
-for coord in coordinates:
-    a, b, c = map(int, coord.split())
-    x += a
-    y += b
-    z += c
-
-if x == 0:
-    if y == 0:
-        if z == 0:
-            print('YES')
+if x == 0 and y == 0 and z ==0:
+    print('YES')
 else:
     print('NO')
