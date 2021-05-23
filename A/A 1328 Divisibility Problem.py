daf021b1b0f96734f4d682a3f1cabe9647b43665
Zielman <gdowskimarcin@gmail.com>
2021-05-23 06:13:01 +0200

    Removed redundancy

--- A/A 1328 Divisibility Problem.py
+++ A/A 1328 Divisibility Problem.py
@@ -1,15 +1,11 @@
 # https://codeforces.com/problemset/problem/1328/A
 
-def main():
-    t = int(input())
-    cases = [list(map(int, input().split())) for c in range(t)]
+t = int(input())
+cases = [list(map(int, input().split())) for _ in range(t)]
 
-    for case in cases:
-        if case[0] % case[1] == 0:
-            steps = 0
-        else:
-            steps = case[1] - (case[0] % case[1])
-        print(steps)
-
-if __name__ == '__main__':
-    main()
+for case in cases:
+    if case[0] % case[1] == 0:
+        steps = 0
+    else:
+        steps = case[1] - (case[0] % case[1])
+    print(steps)
--- A/A 1409 Yet Another Two Integers Problem.py
+++ A/A 1409 Yet Another Two Integers Problem.py
@@ -1,15 +1,11 @@
 # https://codeforces.com/problemset/problem/1409/A
 
-def main():
-    t = int(input())
-    cases = [[int(i) for i in input().split()] for i in range(t)]
-    
-    for case in cases:
-        a, b = case[0], case[1]
-        if (a-b) % 10 == 0:
-            print(abs(a-b)//10)
-        else:
-            print(abs(a-b)//10 + 1)
+t = int(input())
+cases = [[int(i) for i in input().split()] for i in range(t)]
 
-if __name__ == '__main__':
-    main()
+for case in cases:
+    a, b = case[0], case[1]
+    if (a-b) % 10 == 0:
+        print(abs(a-b)//10)
+    else:
+        print(abs(a-b)//10 + 1)
